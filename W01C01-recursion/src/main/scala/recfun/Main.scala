2cd6008aff009343544667911a73abef7da8aa37
Vaillant Samuel <samuel.vllnt@gmail.com>
2017-05-19 16:19:59 +0200

    feat: pascal exercice

--- W01C01-recursion/src/main/scala/recfun/Main.scala
+++ W01C01-recursion/src/main/scala/recfun/Main.scala
@@ -13,8 +13,19 @@ object Main {
   /**
    * Exercise 1
    */
-    def pascal(c: Int, r: Int): Int = ???
-  
+  def pascal(c: Int, r: Int): Int = {
+    def factorial(x: Int): Int = {
+      def loop (acc: Int, n: Int): Int = {
+        if (n == 0) acc
+        else loop(acc * n, n - 1)
+      }
+
+      loop(1, x)
+    }
+
+    factorial(r) / (factorial(c) * factorial(r - c))
+  }
+
   /**
    * Exercise 2
    */
