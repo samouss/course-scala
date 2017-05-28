ba9afbdaa4ba517a390647881c374a4f3b04e341
Vaillant Samuel <samuel.vllnt@gmail.com>
2017-05-28 19:37:04 +0200

    feat(W03): tying everything together

--- W03-oosets/src/main/scala/objsets/TweetSet.scala
+++ W03-oosets/src/main/scala/objsets/TweetSet.scala
@@ -204,14 +204,19 @@ object GoogleVsApple {
   val google = List("android", "Android", "galaxy", "Galaxy", "nexus", "Nexus")
   val apple = List("ios", "iOS", "iphone", "iPhone", "ipad", "iPad")
 
-  lazy val googleTweets: TweetSet = ???
-  lazy val appleTweets: TweetSet = ???
-  
+  def filterTwittersetFromKeywords(tweets: TweetSet, keywords: List[String]): TweetSet = {
+    tweets.filter(tweet => keywords.exists(tweet.text.contains))
+  }
+
+  lazy val googleTweets: TweetSet = filterTwittersetFromKeywords(allTweets, google)
+
+  lazy val appleTweets: TweetSet = filterTwittersetFromKeywords(allTweets, apple)
+
   /**
    * A list of all tweets mentioning a keyword from either apple or google,
    * sorted by the number of retweets.
    */
-  lazy val trending: TweetList = ???
+  lazy val trending: TweetList = googleTweets.union(appleTweets).descendingByRetweet
 }
 
 object Main extends App {
