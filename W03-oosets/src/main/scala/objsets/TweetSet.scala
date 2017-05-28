49127417808af73973864e11cedd5ba869e9104e
Vaillant Samuel <samuel.vllnt@gmail.com>
2017-05-28 18:30:29 +0200

    feat(W03): filtering

--- W03-oosets/src/main/scala/objsets/TweetSet.scala
+++ W03-oosets/src/main/scala/objsets/TweetSet.scala
@@ -41,7 +41,9 @@ abstract class TweetSet {
    * Question: Can we implment this method here, or should it remain abstract
    * and be implemented in the subclasses?
    */
-  def filter(p: Tweet => Boolean): TweetSet = ???
+  def filter(p: Tweet => Boolean): TweetSet = {
+    this.filterAcc(p, new Empty)
+  }
   
   /**
    * This is a helper method for `filter` that propagetes the accumulated tweets.
@@ -108,7 +110,7 @@ abstract class TweetSet {
 
 class Empty extends TweetSet {
 
-  def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = ???
+  def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = acc
   
   /**
    * The following methods are already implemented
@@ -125,8 +127,10 @@ class Empty extends TweetSet {
 
 class NonEmpty(elem: Tweet, left: TweetSet, right: TweetSet) extends TweetSet {
 
-  def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = ???
-  
+  def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = {
+    if (p(elem)) left.filterAcc(p, right.filterAcc(p, acc.incl(elem)))
+    else left.filterAcc(p, right.filterAcc(p, acc))
+  }
     
   /**
    * The following methods are already implemented
