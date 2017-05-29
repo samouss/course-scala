d213ea5a5001d9d703751bbad3a0f3d6664c58e8
Vaillant Samuel <samuel.vllnt@gmail.com>
2017-05-29 10:47:26 +0200

    feat(W03): add function as object demonstration on list

--- course/src/main/scala/W03/List.scala
+++ course/src/main/scala/W03/List.scala
@@ -10,8 +10,14 @@ class Cons[T](val head: T, val tail: List[T]) extends List[T] {
   def isEmpty: Boolean = false
 }
 
-class Nil extends List[T] {
+class Nil[T] extends List[T] {
   def isEmpty: Boolean = true
   def head: Nothing = throw new NoSuchElementException("Nil.head")
   def tail: Nothing = throw new NoSuchElementException("Nil.tail")
+}
+
+object List {
+  def apply[T](): List[T] = new Nil
+  def apply[T](x: T): List[T] = new Cons(x, new Nil)
+  def apply[T](x1: T, x2: T): List[T] = new Cons(x1, new Cons(x2, new Nil))
 }
