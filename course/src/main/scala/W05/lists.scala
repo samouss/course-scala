234db899c2bcb3c5027f16990912db5059a1f79a
Vaillant Samuel <samuel.vllnt@gmail.com>
2017-06-13 19:01:55 +0200

    feat(W05): add reverse function

--- course/src/main/scala/W05/lists.scala
+++ course/src/main/scala/W05/lists.scala
@@ -3,3 +3,8 @@ def init[T](xs: List[T]): List[T] = xs match {
   case _ :: Nil => Nil
   case y :: ys => y :: init(ys)
 }
+
+def reverse[T](xs: List[T]): List[T] = xs match {
+  case Nil => xs
+  case y :: ys => reverse(ys) ::: List(y)
+}
