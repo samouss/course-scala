c6ea95b4ced9a2e4d791d591742179a9335aba48
Vaillant Samuel <samuel.vllnt@gmail.com>
2017-06-23 17:05:21 +0200

    feat(W01): add course on queries with for

---
+++ program-design-in-scala/course/src/main/scala/W01/Books.scala
@@ -0,0 +1,48 @@
+package W01
+
+object Books {
+
+  case class Book(title: String, authors: List[String])
+
+  val books = List(
+    Book(
+      title = "Structure and Interpretation of Computer Programs",
+      authors = List("Abelson, Harald", "Sussman, Gerald J.")
+    ),
+    Book(
+      title = "Introduction to Functional Programming",
+      authors = List("Bird, Richard", "Wadler, Phil")
+    ),
+    Book(
+      title = "Effective Java",
+      authors = List("Bloch, Joshua")
+    ),
+    Book(
+      title = "Effective Java 2",
+      authors = List("Bloch, Joshua")
+    ),
+    Book(
+      title = "Java Puzzlers",
+      authors = List("Bloch, Joshua", "Gafter, Neal")
+    ),
+    Book(
+      title = "Programming in Scala",
+      authors = List("Odersky, Martin", "Spoon, Lex", "Venners, Bill")
+    )
+  )
+
+  for {
+    book <- books
+    author <- book.authors
+    if author.startsWith("Block,")
+  } yield book.title
+
+  for {
+    book1 <- books
+    book2 <- books
+    if book1.title < book2.title
+    author1 <- book1.authors
+    author2 <- book2.authors
+    if author1 == author2
+  } yield author1
+}
